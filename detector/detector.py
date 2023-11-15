d695439fd0cac2b4adab297efa04a83ab58d245a
Hui-En Lin <willakelin@gmail.com>
2023-11-15 14:51:36 +0100

    update: sender is done

--- detector/detector.py
+++ detector/detector.py
@@ -5,6 +5,7 @@ import copy
 import os
 import pathlib
 import define_origin
+import sender
 
 # ChAruco board configs
 PATTERN = (5, 7)
@@ -27,8 +28,8 @@ OBJ_POINTS[3] = np.array([[-MARKER_LENGTH /2.,-MARKER_LENGTH /2., 0]], np.float3
 origin_rvec = None
 origin_tvec = None
 
-
 def detect(frame, cameraMatrix, distCoeffs, origin_rvec, origin_tvec):
+    global WEBSOCKET
     imageCopy = copy.copy(frame)
     detectorParams = aruco.DetectorParameters()
     detector = aruco.ArucoDetector(dictionary=ARUCO_DICT, detectorParams=detectorParams)
@@ -51,17 +52,32 @@ def detect(frame, cameraMatrix, distCoeffs, origin_rvec, origin_tvec):
             (255, 255, 255),
             1)
         
+        rotations = [None] * markerCount
         rvecs = [None] * markerCount
         tvecs = [None] * markerCount
 
         for i in range(markerCount):
             retval, rvecs[i], tvecs[i] = cv2.solvePnP(OBJ_POINTS, markerCorners[i], cameraMatrix, distCoeffs)
+            # Convert rotation vector to rotation matrix
+            rot_mat, _ = cv2.Rodrigues(rvecs[i])
+
+            # Extract pitch, yaw, and roll from the rotation matrix
+            pitch = np.arcsin(-rot_mat[1, 2])
+            yaw = np.arctan2(rot_mat[0, 2], rot_mat[2, 2])
+            roll = np.arctan2(rot_mat[1, 0], rot_mat[1, 1])
+
+            # Convert angles from radians to degrees if needed
+            pitch_degrees = np.degrees(pitch)
+            yaw_degrees = np.degrees(yaw)
+            roll_degrees = np.degrees(roll)
+            rotations[i] = np.array([pitch_degrees, yaw_degrees, roll_degrees], np.float32)
         for i in range(markerCount):
             cv2.drawFrameAxes(imageCopy, cameraMatrix, distCoeffs, rvecs[i], tvecs[i], 0.5)
         
-        if markerCount > 1:
+        sender.send_object_data(WEBSOCKET, markerIds, tvecs, rotations)
+        # if markerCount > 1:
             # Convert rotation vector to rotation matrix
-            rot_mat, _ = cv2.Rodrigues(rvecs[0])
+            # rot_mat, _ = cv2.Rodrigues(rvecs[0])
 
             # Extract pitch, yaw, and roll from the rotation matrix
             # pitch = np.arcsin(-rot_mat[1, 2])
@@ -74,7 +90,7 @@ def detect(frame, cameraMatrix, distCoeffs, origin_rvec, origin_tvec):
             # roll_degrees = np.degrees(roll)
         
             # print(f'pitch_degrees: {pitch_degrees} yaw_degrees: {yaw_degrees} roll_degrees: {roll_degrees}')
-            print(f'tvec_0: {np.array_str(tvecs[0], precision=3, suppress_small=True)} tvec_1: {np.array_str(tvecs[1], precision=3, suppress_small=True)}')
+            # print(f'tvec_0: {np.array_str(tvecs[0], precision=3, suppress_small=True)} tvec_1: {np.array_str(tvecs[1], precision=3, suppress_small=True)}')
     else:
         cv2.putText( 
             imageCopy,
@@ -109,9 +125,9 @@ def run(cameraMatrix, distCoeffs):
     while cap.isOpened():
         isCaptured, frame = cap.read()
         
-        if (origin_rvec is None) and (origin_tvec is None):
-            origin_rvec, origin_tvec = define_origin.define_origin(frame, cameraMatrix, distCoeffs)
-            continue
+        # if (origin_rvec is None) and (origin_tvec is None):
+        #     origin_rvec, origin_tvec = define_origin.define_origin(frame, cameraMatrix, distCoeffs)
+        #     continue
         if isCaptured:
             detect(frame, cameraMatrix, distCoeffs, origin_rvec, origin_tvec)
             
@@ -121,10 +137,12 @@ def run(cameraMatrix, distCoeffs):
     
 
 if __name__ == "__main__":
+    global WEBSOCKET
     path = f'{pathlib.Path().resolve()}/inputs/calib.npz'
     with np.load(path) as X:
         cameraMatrix, distCoeffs = [X[i] for i in ("cameraMatrix", "distCoeffs")]
 
+    WEBSOCKET = sender.setup_websocket_client()
     # print(cameraMatrix)
     # print(distCoeffs)
     run(cameraMatrix, distCoeffs)
---
+++ detector/sender.py
@@ -0,0 +1,34 @@
+import websocket
+import json 
+
+def setup_websocket_client():
+    # ws = websocket.create_connection("ws://localhost:8080")
+    ws = websocket.create_connection("ws://10.2.40.112:8080/")
+    return ws
+
+def format(v):
+    return "{:.2f}".format(round(v, 2))
+
+def send_object_data(ws: websocket.WebSocket(), markerIds, positions, rotations):
+    data = ''
+    for id,p,r in zip(markerIds, positions, rotations):
+        markerId = id[0]
+        x = p[0][0]
+        y = p[1][0]
+        z = p[2][0]
+        
+        pitch = r[0]
+        yaw = r[1]
+        roll = r[2]
+        
+        data += f'Box{markerId}'
+        data += f'[{format(x)};{format(y)};{format(z)}]'
+        data += f'/'
+        data += f'[{format(pitch)};{format(yaw)};{format(roll)}]'
+        data += f'Box{markerId}end/'
+        
+    message = {
+        "raw": data
+    }
+    ws.send(json.dumps(message))
+    print(data)
