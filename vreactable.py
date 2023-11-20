a226bede51ba65c703ea425211b410e5068546c1
Hui-En Lin <willakelin@gmail.com>
2023-11-20 14:20:55 +0100

    update: markup gui

--- vreactable.py
+++ vreactable.py
@@ -1,13 +1,27 @@
-import cv2
+#!/usr/bin/python3
+import pathlib
+import pygubu
+PROJECT_PATH = pathlib.Path(__file__).parent
+PROJECT_UI = PROJECT_PATH / "vreactable.ui"
+
+
+class VreactableApp:
+    def __init__(self, master=None):
+        self.builder = builder = pygubu.Builder()
+        builder.add_resource_path(PROJECT_PATH)
+        builder.add_from_file(PROJECT_UI)
+        # Main widget
+        self.mainwindow = builder.get_object("toplevel_vreactable", master)
+
+        self.is_calibrated = None
+        builder.import_variables(self, ['is_calibrated'])
+
+        builder.connect_callbacks(self)
+
+    def run(self):
+        self.mainwindow.mainloop()
+
 
 if __name__ == "__main__":
-    markerImage = None
-    dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_50)
-    markerImage = cv2.aruco.generateImageMarker(dictionary, 23, 200, markerImage, 1)
-    print(markerImage)
-    cv2.imshow("Image", markerImage)
-
-    while True:
-        if cv2.waitKey(1) == 27:
-            break
-        
+    app = VreactableApp()
+    app.run()
